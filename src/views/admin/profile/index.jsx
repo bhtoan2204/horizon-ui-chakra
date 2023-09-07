544876d6c884dd27a286d0cfd77675be35b666a0
Bành Hảo Toàn <86637323+bhtoan2204@users.noreply.github.com>
2023-09-07 15:44:56 +0700

    modify

--- src/views/admin/profile/index.jsx
+++ src/views/admin/profile/index.jsx
@@ -25,9 +25,6 @@ import { Box, Grid } from "@chakra-ui/react";
 
 // Custom components
 import Banner from "views/admin/profile/components/Banner";
-import General from "views/admin/profile/components/General";
-import Notifications from "views/admin/profile/components/Notifications";
-import Projects from "views/admin/profile/components/Projects";
 import Storage from "views/admin/profile/components/Storage";
 import Upload from "views/admin/profile/components/Upload";
 
